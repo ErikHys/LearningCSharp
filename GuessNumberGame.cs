f8b4287a3cd349fd0cde8e95c3c1f75fee940c5b
ErikHys <E.Hystad@student.uib.no>
2021-01-06 14:37:40 +0100

    number guessing game complete

--- GuessNumberGame.cs
+++ GuessNumberGame.cs
@@ -2,12 +2,34 @@
 
 namespace HelloWorld
 {
+    /**
+     * Simple number guessing game.
+     */
     public static class GuessNumberGame
     {
-        public static void Test()
+        public static void Run()
         {
-            Console.Write("Static class");
-            //Test commit
+            Random r = new Random();
+            int winNumber = r.Next(0, 100);
+            bool won = false;
+            do
+            {
+                Console.Write("Guess a number: ");
+                int guess = int.Parse(Console.ReadLine());
+                if (guess > winNumber)
+                {
+                    Console.Write("Too high!");
+                }else if (guess < winNumber)
+                {
+                    Console.Write("Too low!");
+                }
+                else
+                {
+                    won = true;
+                }
+                
+            } while (!won);
+            Console.Write("You won!");
         }
     }
 }
